6959a46ced61c8325aefda5d21c857b82503d3e3
Amita <sardana.amita2@gmail.com>
2020-11-04 13:29:08 +0530

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -25,16 +25,6 @@ ActiveRecord::Schema.define(version: 2020_10_30_122820) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
-    t.boolean "confirmed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
-  end
-
   create_table "likes", force: :cascade do |t|
     t.integer "post_id"
     t.integer "user_id"
@@ -63,7 +53,4 @@ ActiveRecord::Schema.define(version: 2020_10_30_122820) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "friendships", "users"
-  add_foreign_key "friendships", "users", column: "friend_id"
 end
