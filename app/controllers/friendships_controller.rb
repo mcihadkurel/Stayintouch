176c377e8d24a545aedb542413cd9eaa381a2847
Amita <sardana.amita2@gmail.com>
2020-11-04 16:11:50 +0530

    Add inverse friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,6 @@
 class FriendshipsController < ApplicationController
+  after_action :create_inverse_friendship, :delete_inverse_friendship
+
   def create
     @friendship = current_user.friendships.new(friend_id: params[:friend_id])
     @friendship.save
@@ -10,5 +12,17 @@ class FriendshipsController < ApplicationController
     current_user.friendships.delete(@friendship)
 
     redirect_to users_path, notice: 'You have unfriended the user successfully'
+  end
+
+  private
+
+  def create_inverse_friendship
+    @inverse = Friendship.new(user_id: params[:friend_id], friend_id: current_user.id)
+    @inverse.save
+  end
+
+  def delete_inverse_friendship
+    @inverse = Friendship.where(friend_id: current_user.id, user_id: params[:id])
+    Friendship.delete(@inverse)
   end
 end
