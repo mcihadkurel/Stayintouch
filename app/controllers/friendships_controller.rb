aa7457eb2da150c13b5a453ea99f450ce43492fd
Amita <sardana.amita2@gmail.com>
2020-11-04 19:34:54 +0530

    Add after_create and after_destroy hook

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,28 +1,17 @@
 class FriendshipsController < ApplicationController
-  after_action :create_inverse_friendship, :delete_inverse_friendship
+  #   after_action :create_inverse_friendship,
 
   def create
-    @friendship = current_user.friendships.new(friend_id: params[:friend_id])
-    @friendship.save
+    friendship = current_user.friendships.new(friend_id: params[:friend_id])
+    friendship.save
     redirect_to users_path, notice: 'You have successfully sent a connection request'
   end
 
   def destroy
-    @friendship = current_user.friendships.where(friend_id: params[:id])
-    current_user.friendships.delete(@friendship)
+    # friendship = current_user.friendships.where(friend_id: params[:id])
+    Friendship.where(friend_id: params[:id], user_id: current_user.id).first.destroy
+    # current_user.friendships.delete(@friendship)
 
     redirect_to users_path, notice: 'You have unfriended the user successfully'
-  end
-
-  private
-
-  def create_inverse_friendship
-    @inverse = Friendship.new(user_id: params[:friend_id], friend_id: current_user.id)
-    @inverse.save
-  end
-
-  def delete_inverse_friendship
-    @inverse = Friendship.where(friend_id: current_user.id, user_id: params[:id])
-    Friendship.delete(@inverse)
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,27 @@
 class Friendship < ApplicationRecord
+  # after_create :create_inverse_friendship
+  # after_destroy :delete_inverse_friendship
+
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  # private
+
+  # def create_inverse_friendship
+  #   @inverse = Friendship.new(user_id: params[:friend_id], friend_id: current_user.id)
+  #   @inverse.save
+  # end
+
+  # def delete_inverse_friendship
+  #   @inverse = Friendship.where(friend_id: current_user.id, user_id: params[:id])
+  #   Friendship.delete(@inverse)
+  # end
+
+  after_create do |f|
+    Friendship.create!(user_id: f.friend_id, friend_id: f.user_id) unless Friendship.where(friend_id: f.user_id).first
+  end
+
+  after_destroy do |f|
+    Friendship.where(friend_id: f.user_id, user_id: f.friend_id).delete_all
+  end
 end
