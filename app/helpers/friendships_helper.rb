f174aa7405ca2a99186d75590946328ef2bc71bb
Amita <sardana.amita2@gmail.com>
2020-11-05 17:57:55 +0530

    Add mutual connection

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,11 +1,15 @@
 module FriendshipsHelper
   private
 
-  def is_friend?(user1, user2)
-    user1.friends.include?(user2)
-  end
+  # def is_friend?(user1, user2)
+  #   user1.friends.include?(user2)
+  # end
 
   def friends(user)
     user.friendships.select(&:confirmed)
+  end
+
+  def mutual_friends(user1, user2)
+    user1.friends & user2.friends
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships
-  has_many :friends, through: :friendships, foreign_key: 'friend_id'
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships, :source => :user, foreign_key: 'friend_id'
 end
