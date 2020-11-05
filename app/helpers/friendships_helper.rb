723f33555bf6075f2097eff816e029ef85efdfb4
Amita <sardana.amita2@gmail.com>
2020-11-05 18:34:17 +0530

    Update friends status

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -9,6 +9,11 @@ module FriendshipsHelper
     user.friendships.select(&:confirmed)
   end
 
+  def are_friends?(current_user, user)
+    friendships = friends(current_user)
+    friendships.find_index { |f| f.friend_id == user.id }
+  end
+
   def mutual_friends(user1, user2)
     user1.friends & user2.friends
   end
