43c21a6f91c99bc6be7f7ded50c15eab9fafb954
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-11-09 17:19:33 +0300

    Add login user before each example

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Friendship', type: :request do
+    describe 'request list of all users' do
+        before(:each) do
+          @user = FactoryBot.create(:user)
+          login_as(@user, scope: :user)
+        end
+    
 end
