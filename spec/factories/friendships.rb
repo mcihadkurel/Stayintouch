2e33b6e20686c43149d1dd98fcabcb12055440b1
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-11-09 17:17:02 +0300

    Add friendship factory

--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -1,2 +1,7 @@
 FactoryBot.define do
+    factory :friendship do
+        user { FactoryBot.create(:user) }
+        friend { FactoryBot.create(:second_user) }
+        confirmed { true }
+      end
 end
