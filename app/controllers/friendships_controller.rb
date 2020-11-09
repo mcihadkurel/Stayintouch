fd30347d8f68a456f0ad0ebaa4326cd2f9a1d0d4
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-11-09 23:59:24 +0300

    Change action values

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,13 +2,13 @@ class FriendshipsController < ApplicationController
   #   after_action :create_inverse_friendship,
 
   def create
-    friendship = current_user.friendships.new(friend_id: params[:friend_id], confirmed: true)
+    friendship = current_user.friendships.new(friend_id: params[:friend_id], confirmed: false)
     friendship.save
     redirect_to users_path, notice: 'You have successfully sent a connection request'
   end
 
   def update
-    @friendship = Friendship.where(user_id: current_user.id, friend_id: params[:id], confirmed: false)
+    @friendship = Friendship.where(user_id: current_user.id, friend_id: params[:id], confirmed: true)
     @friendship.update(confirmed: true)
     redirect_to current_user
   end
