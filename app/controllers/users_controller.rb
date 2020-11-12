7c8b05dface09fc558f8b70ba4f0537bb9f1ef5d
Amita <sardana.amita2@gmail.com>
2020-11-12 14:07:43 +0530

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,25 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    @pending_requests = @user.friendships.where(confirmed: false)
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+
+    if current_user.confirm_friend(@user)
+      redirect_to users_path, notice: 'Friend request accepted.'
+    else
+      redirect_to users_path, alert: 'Something went wrong with accepting the request.'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+
+    if current_user.reject_request(@user)
+      redirect_to users_path, notice: 'Friendship request rejected successfully.'
+    else
+      redirect_to users_path, alert: 'Something went wrong with the rejection.'
+    end
   end
 end
