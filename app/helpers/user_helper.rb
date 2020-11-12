487ffdea0f6457f09774865cd225fe86afbf144e
Amita <sardana.amita2@gmail.com>
2020-11-12 16:13:26 +0530

    update button style

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,17 +4,17 @@ module UserHelper
 
     out = ''
     if current_user.friend?(obj)
-      out << link_to('Unfriend', unfriend_path(user_id: obj.id), class: 'profile-link f-link', method: :delete)
+      out << link_to('Unfriend', unfriend_path(user_id: obj.id), class: 'btn btn-secondary', method: :delete)
     elsif current_user.pending_friends.include?(obj)
       out << link_to('pending friendship', '#', class: 'profile-link f-link')
       out << ' | '
-      out << link_to('cancel', unfriend_path(user_id: obj.id), class: 'profile-link f-link', method: :delete)
+      out << link_to('cancel', unfriend_path(user_id: obj.id), class: 'btn btn-secondary', method: :delete)
     elsif current_user.friend_requests.include?(obj)
-      out << link_to('Accept', invite_path(user_id: obj.id), class: 'profile-link f-link', method: :put)
+      out << link_to('Accept', invite_path(user_id: obj.id), class: 'btn btn-primary', method: :put)
       out << ' | '
-      out << link_to('Reject', reject_path(user_id: obj.id), class: 'profile-link f-link', method: :delete)
+      out << link_to('Reject', reject_path(user_id: obj.id), class: 'btn btn-secondary', method: :delete)
     else
-      out << link_to('Send friend request', invite_path(user_id: obj.id), class: 'profile-link f-link', method: :post)
+      out << link_to('Add Friend', invite_path(user_id: obj.id), class: 'btn btn-primary', method: :post)
     end
 
     out.html_safe
